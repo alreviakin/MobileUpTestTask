da8a5896c25d038c7918271cdf53f01f2ad3e52d
aleksejrevakin <alexey.amahasla@gmail.com>
2023-04-21 17:58:00 +0300

    Create galleryController

---
+++ MobileUpTestTask/Controllers/GalleryController.swift
@@ -0,0 +1,27 @@
+//
+//  GalleryController.swift
+//  MobileUpTestTask
+//
+//  Created by Алексей Ревякин on 21.04.2023.
+//
+
+import UIKit
+
+class GalleryController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+        layout()
+    }
+    
+    func configure() {
+        
+    }
+    
+    func layout() {
+        
+    }
+    
+}
