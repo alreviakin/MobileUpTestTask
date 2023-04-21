bc2a83375d3f8cccbcc9f34cf54b4817801330bd
aleksejrevakin <alexey.amahasla@gmail.com>
2023-04-21 16:51:21 +0300

    Create project

---
+++ MobileUpTestTask/MobileUpTestTask/Controllers/AuthorizationController.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorizationController.swift
+//  MobileUpTestTask
+//
+//  Created by Алексей Ревякин on 21.04.2023.
+//
+
+import UIKit
+
+class AuthorizationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
