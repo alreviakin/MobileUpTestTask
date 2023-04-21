daa85a2ffe452d42774a6df54cf854ecf85630d9
aleksejrevakin <alexey.amahasla@gmail.com>
2023-04-21 17:48:54 +0300

    Configure api VK

--- MobileUpTestTask/Application/AppDelegate.swift
+++ MobileUpTestTask/Application/AppDelegate.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import VK_ios_sdk
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- MobileUpTestTask/Application/SceneDelegate.swift
+++ MobileUpTestTask/Application/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import VK_ios_sdk
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate, AuthServiceDelegate {
 
@@ -16,8 +17,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, AuthServiceDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
+        AuthService.shared.delegate = self
         window?.rootViewController = AuthorizationController()
         window?.makeKeyAndVisible()
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            print(url)
+            VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
+        }
     }
     
     func authServiceShouldPresent(viewController: UIViewController) {
